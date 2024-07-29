5339256e40bb27063fa2bfa1e7cf4b4d6d23a56b
yuyuyu <g023c1247@g.neec.ac.jp>
2024-07-29 09:58:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-var busLocation = [35.6247, 139.3536]; // バス停の新しい座標
+var busLocation = [35.62519493525218, 139.34105785827416]; // バス停の新しい座標
 L.marker(busLocation).addTo(map)
     .bindPopup('バスの位置')
     .openPopup();
-
