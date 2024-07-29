31978f135d4c6a137a6aac417f16d441c6007f67
yuyuyu <g023c1247@g.neec.ac.jp>
2024-07-29 09:48:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var map = L.map('map').setView([35.642, 139.317], 16); // 日本工学院八王子専門学校の図書館棟前の座標
+var map = L.map('map').setView([35.6249, 139.3533], 16); // 東京工科大学の図書館棟の座標
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-var busLocation = [35.642, 139.317]; // バス停の新しい座標
+var busLocation = [35.6249, 139.3533]; // バス停の新しい座標
 L.marker(busLocation).addTo(map)
     .bindPopup('バスの位置')
     .openPopup();
