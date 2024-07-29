1df1d150bc1e104624afd7e3abf2a2efeec4a768
yuyuyu <g023c1247@g.neec.ac.jp>
2024-07-29 10:53:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-var map = L.map('map').setView([35.62519493525218, 139.34105785827416], 16); // 東京工科大学の図書館棟の座標
+var map = L.map('map').setView([35.62702341561639, 139.33898667028086], 14); // 中心座標を設定
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-var busLocation = [35.62519493525218, 139.34105785827416]; // バス停の新しい座標
-L.marker(busLocation).addTo(map)
-    .bindPopup('バスの位置')
-    .openPopup();
+var busLocations = [
+    [35.62702341561639, 139.33898667028086],
+    [35.62519493525218, 139.34105785827416],
+    [35.63149875364993, 139.32910313903676],
+    [35.654351868130796, 139.33909241912187]
+];
+
+busLocations.forEach(function(location) {
+    L.marker(location).addTo(map)
+        .bindPopup('バスの位置')
+        .openPopup();
+});
