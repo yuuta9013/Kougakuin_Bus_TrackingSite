f96df704c00e37db7319c0294b16ce6ff43fa783
yuyuyu <g023c1247@g.neec.ac.jp>
2024-07-29 11:10:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var busLocations = [
-    [35.62702341561639, 139.33898667028086],
-    [35.62519493525218, 139.34105785827416],
-    [35.63149875364993, 139.32910313903676],
-    [35.654351868130796, 139.33909241912187]
+    { coords: [35.62702341561639, 139.33898667028086], name: '八王子駅行き' },
+    { coords: [35.62519493525218, 139.34105785827416], name: 'みなみ野駅行き' },
+    { coords: [35.63149875364993, 139.32910313903676], name: '学校行き(みなみ野)' },
+    { coords: [35.654351868130796, 139.33909241912187], name: '学校行き(八王子)' }
 ];
 
 busLocations.forEach(function(location) {
-    L.marker(location).addTo(map)
-        .bindPopup('バスの位置');
+    L.marker(location.coords).addTo(map)
+        .bindPopup(location.name);
 });
 
 // 地図の表示範囲をすべてのマーカーが表示されるように調整
-var bounds = L.latLngBounds(busLocations);
+var bounds = L.latLngBounds(busLocations.map(function(location) { return location.coords; }));
 map.fitBounds(bounds);
+
+// バス停の選択機能を追加
+document.getElementById('busStopSelect').addEventListener('change', function(e) {
+    var selectedLocation = busLocations[e.target.value];
+    map.setView(selectedLocation.coords, 16);
+    L.popup()
+        .setLatLng(selectedLocation.coords)
+        .setContent(selectedLocation.name)
+        .openOn(map);
+});
