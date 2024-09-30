d51241ce68522437f72468e6427e070c2c1b8c76
yuyuyu <g023c1247@g.neec.ac.jp>
2024-09-30 10:35:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// script.js
+
 var map = L.map('map').setView([35.62702341561639, 139.33898667028086], 14); // 中心座標を設定
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -39,3 +41,51 @@ document.getElementById('busStopSelect').addEventListener('change', function(e) 
 document.getElementById('notificationOptions').addEventListener('click', function() {
     window.location.href = 'notification.html';
 });
+
+// バスの移動経路を定義
+var routes = {
+    '学校行き(みなみ野)': [
+        [35.63149875364993, 139.32910313903676], // 学校行き(みなみ野)
+        [35.62702341561639, 139.33898667028086], // 八王子駅行き
+        [35.62519493525218, 139.34105785827416], // みなみ野駅行き
+        [35.62702341561639, 139.33898667028086]  // 学校行き(みなみ野)に戻る
+    ],
+    '学校行き(八王子)': [
+        [35.654351868130796, 139.33909241912187], // 学校行き(八王子)
+        [35.62702341561639, 139.33898667028086],  // 八王子駅行き
+        [35.654351868130796, 139.33909241912187]  // 学校行き(八王子)に戻る
+    ]
+};
+
+// バスのマーカーを作成
+var buses = {
+    'みなみ野行きバス': L.marker(routes['学校行き(みなみ野)'][0], {icon: L.icon({
+        iconUrl: 'images/bus-icon.png', // パスを修正
+        iconSize: [25, 25],
+        iconAnchor: [12, 12]
+    })}).addTo(map).bindPopup('みなみ野行きバス'),
+
+    '八王子行きバス': L.marker(routes['学校行き(八王子)'][0], {icon: L.icon({
+        iconUrl: 'images/bus-icon.png', // パスを修正
+        iconSize: [25, 25],
+        iconAnchor: [12, 12]
+    })}).addTo(map).bindPopup('八王子行きバス')
+};
+
+// バスの移動関数
+function moveBus(busName, route, index = 0) {
+    if (index >= route.length) {
+        index = 0; // ルートの最後に達したら最初に戻る
+    }
+
+    buses[busName].setLatLng(route[index]);
+
+    // 次の移動を設定（例: 2秒ごとに移動）
+    setTimeout(function() {
+        moveBus(busName, route, index + 1);
+    }, 2000);
+}
+
+// バスの移動を開始
+moveBus('みなみ野行きバス', routes['学校行き(みなみ野)']);
+moveBus('八王子行きバス', routes['学校行き(八王子)']);
