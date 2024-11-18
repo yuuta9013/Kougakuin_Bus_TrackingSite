27e3cee2cb8c4f3760c50d16955b1dfb290b888f
yuyuyu <g023c1247@g.neec.ac.jp>
2024-11-18 10:09:21 +0900

    Update game-script.js

--- game-script.js
+++ game-script.js
@@ -5,7 +5,7 @@ let isSpinning = [false, false, false];
 
 function startSpin() {
   for (let i = 1; i <= 3; i++) {
-    const reel = document.getElementById(`reel${i}`);
+    const reel = document.getElementById(`reel${i}`);　
     reel.classList.add("spinning");
     document.getElementById(`stop${i}`).disabled = false;
     isSpinning[i - 1] = true;
